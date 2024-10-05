d145edd89a1ca5f9f71a15a5b3ce2b0be7feae06
Jorge Andres Salgado Echeverria <jsalgadoecheverria@gmail.com>
2024-10-04 19:57:38 -0500

    configuracion de variables de entorno

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import config from './config/configuration';
+import * as Joi from 'joi';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [config],
+      // validationSchema:Joi.object({
+
+      // })
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
