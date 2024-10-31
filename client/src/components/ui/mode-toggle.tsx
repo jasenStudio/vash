c7af8c416169b4d737661fa0ae6e1df50314d789
Jorge Andres Salgado Echeverria <jasen.studio@gmail.com>
2024-10-31 11:38:52 -0500

    create registerpage

--- client/src/components/ui/mode-toggle.tsx
+++ client/src/components/ui/mode-toggle.tsx
@@ -16,8 +16,14 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun
+            className="h-[2rem] w-[2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+            style={{ height: "1.4rem", width: "1.4rem" }}
+          />
+          <Moon
+            className="absolute h-[2rem] w-[2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+            style={{ height: "1.4rem", width: "1.4rem" }}
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
--- client/src/vash/auth/pages/RegisterPage.tsx
+++ client/src/vash/auth/pages/RegisterPage.tsx
@@ -1,3 +1,46 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Link } from "react-router-dom";
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <>
+      <div className="w-full sm:w-[450px]">
+        <div className="absolute ml-10 right-5 top-5">
+          <ModeToggle />
+        </div>
+
+        <div className="pl-10 mb-10">
+          <h2 className="text-4xl font-bold">Crear Cuenta</h2>
+          <span> ¿Ya tienes una cuenta?</span>
+          <Link
+            className="ml-2 text-gray-700 dark:text-gray-400 font-semibold "
+            to="/sign-in"
+          >
+            Iniciar sesión
+          </Link>
+        </div>
+        <div>
+          <label htmlFor="email">email</label>
+          <input type="email" id="email" />
+        </div>
+
+        <div>
+          <label htmlFor="usuario">usuario</label>
+          <input type="text" id="usuario" />
+        </div>
+
+        <div>
+          <label htmlFor="contraseña">contraseña</label>
+          <input type="email" id="contraseña" />
+        </div>
+
+        <div>
+          <label htmlFor="confirmPassword">confirmar contraseña</label>
+          <input type="password" id="confirmPassword" />
+        </div>
+
+        <button>Crear Cuenta</button>
+      </div>
+    </>
+  );
 };
